69d9d45d7f5ae5ad42d6cd422e5422db05a8134c
xiyue <xiyue@qunhemail.com>
2017-10-24 20:04:08 +0800

    test branch

--- examples/shopping-cart/src/components/Cart.js
+++ examples/shopping-cart/src/components/Cart.js
@@ -24,7 +24,7 @@ const Cart  = ({ products, total, onCheckoutClicked }) => {
       <p>Total: &#36;{total}</p>
       <button onClick={onCheckoutClicked}
         disabled={hasProducts ? '' : 'disabled'}>
-        结账1
+        结账1test
       </button>
     </div>
   )
